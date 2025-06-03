56ffc0d0708a4df2eb7476901ff429e11fa0f494
Humphrey Joshua <joshuahumphrey579@gmail.com>
2025-06-03 07:32:55 +0100

    removed prisma generated files

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -1,21 +1,8 @@
 import { Prisma, PrismaClient } from './generated';
 
-let prisma: PrismaClient;
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-if (process.env.NODE_ENV === 'production') {
-	prisma = new PrismaClient({
-		log: ['error'],
-	});
-} else {
-	// @ts-expect-error @typescript-eslint/ban-ts-comment
-	if (!global.prisma) {
-		// @ts-expect-error @typescript-eslint/ban-ts-comment
-		global.prisma = new PrismaClient({
-			log: ['error'],
-		});
-	} // @ts-expect-error @typescript-eslint/ban-ts-comment
-	prisma = global.prisma;
-}
+const prisma = new PrismaClient().$extends(withAccelerate());
 
 export * from './generated';
 export default prisma;
