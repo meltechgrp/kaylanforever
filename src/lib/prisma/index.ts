8942b0ad5107ab1716eadfc1366f0aef6a49d943
Humphrey Joshua <joshuahumphrey579@gmail.com>
2025-06-03 08:03:41 +0100

    Update index.ts

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -1,8 +1,6 @@
 import { Prisma, PrismaClient } from './generated/edge';
 
-import { withAccelerate } from '@prisma/extension-accelerate';
-
-const prisma = new PrismaClient().$extends(withAccelerate());
+const prisma = new PrismaClient();
 
 export * from './generated';
 export default prisma;
